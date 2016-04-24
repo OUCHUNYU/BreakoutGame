994074e43d7917671a7ac612d81081f9572e99b6
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-24 15:53:52 -0700

    Add layout

---
+++ javascripts/application.js
@@ -0,0 +1,61 @@
+var paddle;
+var bricks = [];
+
+function layout() {
+  GameArea.start();
+  paddle = new Paddle(150, 10, "black", 50, 650);
+  for (var r = 0; r < 200; r += 40) {
+    for(var c = 0; c < 1200; c += 100) {
+      bricks.push(new Brick(80, 30, "green", c + 10, r + 10));
+    }
+  }
+}
+
+var GameArea = {
+  canvas: document.createElement("canvas"),
+  start: function() {
+    this.canvas.width = 1200;
+    this.canvas.height = 700;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+    //calling updateGameArea every 20th milsecond
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+};
+
+function Paddle(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+      ctx = GameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+}
+
+function Brick(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = GameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+}
+
+function updateGameArea() {
+  if (paddle.x >= 1100) {
+    paddle.x = 50;
+  }
+  paddle.x += 1;
+  GameArea.clear();
+  paddle.update();
+  bricks.forEach(function(brick) { brick.update(); })
+}
