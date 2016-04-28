40a8ba8b938a5b0a030e47d5a9998a8d99a0aed5
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-28 15:08:10 -0700

    Finish

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,6 +2,7 @@ var paddle;
 var ball;
 var bricks = [];
 var padding = 1;
+var score;
 // setup all elements
 function layout() {
   GameArea.start();
@@ -16,14 +17,17 @@ function start() {
     }
   }
   ball = new Ball();
+  score = new Score();
 }
 
 // draw canvas
 var GameArea = {
   canvas: document.createElement("canvas"),
   start: function() {
-    this.canvas.width = 1200;
-    this.canvas.height = 700;
+    // canvas.width = window.innerWidth + "px";
+    // canvas.height = window.innerHeight + "px";
+    this.canvas.width = 1220;
+    this.canvas.height = 750;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 10);
@@ -59,6 +63,7 @@ function Brick(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
+  this.broken = false;
   this.update = function() {
     if (this.x < ball.x + ball.r && ball.x < (this.x + this.width) && this.y + this.height > ball.y && ball.y > this.y   ) {
       ball.dy = -ball.dy;
@@ -66,6 +71,8 @@ function Brick(width, height, color, x, y) {
       this.height = 0;
       this.x = 0;
       this.y = 0;
+      this.broken = true;
+      score.current += 10
     }
 
     ctx = GameArea.context;
@@ -81,26 +88,40 @@ function Ball() {
   this.dy = -3;
   this.x = 200;
   this.y = 630;
-  this.update = function() {
-    if (this.x + this.dx > 1200 || this.x + this.dx < 0) {
-      this.dx = -this.dx;
+}
+
+Ball.prototype.update = function() {
+  if (this.x + this.dx > GameArea.canvas.width || this.x + this.dx < 0) {
+    this.dx = -this.dx;
+  }
+  if(this.y + this.dy < 0) {
+    this.dy = -this.dy;
+  }else if((this.x > paddle.x && this.x <= paddle.x + paddle.width) && (this.y < paddle.y + paddle.height && this.y >= paddle.y - 18)) {
+    this.dx = 8 * ((this.x - (paddle.x + paddle.width / 2)) / paddle.width);
+    this.dy = -this.dy;
+  }else if(this.y + this.dy > GameArea.canvas.height) {
+    clearInterval(GameArea.interval);
+    alert("GAME OVER");
+    location.reload();
     }
-    if(this.y + this.dy < 0) {
-      this.dy = -this.dy;
-    }else if((this.x > paddle.x && this.x <= paddle.x + paddle.width) && (this.y < paddle.y + paddle.height && this.y >= paddle.y - 18)) {
-      this.dx = 8 * ((this.x - (paddle.x + paddle.width / 2)) / paddle.width);
-      this.dy = -this.dy;
-    }else if(this.y + this.dy > 700) {
-      clearInterval(GameArea.interval);
-      }
-    this.x += this.dx;
-    this.y += this.dy;
-    ctx = GameArea.context;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fillStyle = 'red';
-    ctx.fill();
+  this.x += this.dx;
+  this.y += this.dy;
+  ctx = GameArea.context;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fillStyle = 'red';
+  ctx.fill();
+};
+
+// text display
+
+function Score() {
+  this.current = 0;
+  this.update = function() {
+    var ctx = GameArea.context;
+    ctx.font = "30px Arial";
+    ctx.fillText("Score: " + this.current, 10, 720);
   };
 }
 
@@ -108,11 +129,19 @@ function Ball() {
 // animation
 function updateGameArea() {
   GameArea.clear();
+  if(bricks.forEach(anyBricks)) {
+    alert("You won! Good job!");
+        location.reload();
+  }
   if(GameArea.x && GameArea.y) {
     paddle.x = GameArea.x;
   }
   ball.update();
   bricks.forEach(function(brick) { brick.update(); })
-
   paddle.update();
+  score.update();
+}
+
+function anyBricks(brick) {
+  return brick.broken == true;
 }
